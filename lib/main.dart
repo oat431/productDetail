f62edf4f3185c78e417eae556701f3793d3ffbc1
Oralita <oat431@gmail.com>
2021-04-06 15:59:05 +0700

    make it more easier to maintainable

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'component/detail_bar.dart';
-import 'component/detail_item.dart';
-import 'component/detail_info.dart';
-import 'component/detail_price.dart';
+import 'package:product_detail/screen/detail_main.dart';
+import 'package:product_detail/screen/detail_chat_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    ProductDetailPage.tag: (context) => ProductDetailPage(),
+    ProductChatScreen.tag: (context) => ProductChatScreen()
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,59 +18,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'SFProText-Regular',
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: DetailBar(),
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.all(25.0),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    child: DetailItem(),
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    child: Image.asset('assets/icon/Arrow - Left 2_0.png'),
-                  ),
-                  Container(
-                    child: Center(
-                      child: Column(
-                        children: [
-                          SizedBox(height: 60),
-                          DetailPrice(color: Colors.red),
-                          DetailPrice(color: Colors.red),
-                          DetailPrice()
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              DetailInfo()
-            ]),
-      ),
+      home: ProductDetailPage(),
+      routes: routes,
     );
   }
 }
